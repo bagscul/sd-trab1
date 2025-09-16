5272c302f991e5aaad964c91fd6cc797c4bdd581
Lucas Oliveira <luckass1088@gmail.com>
2025-09-15 23:31:06 -0300

    Update ClienteTCP.java

--- ClienteTCP.java
+++ ClienteTCP.java
@@ -1,29 +1,37 @@
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 public class ClienteTCP {
     private static final String SERVER_IP = "192.168.0.1";
     private static final int PORTA = 5000;
+    private static final int NUM_THREADS = 5; // número de threads simultâneas
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) { // 3 threads de exemplo
+        Random rand = new Random();
+
+        for (int i = 1; i <= NUM_THREADS; i++) {
             int threadId = i;
             new Thread(() -> {
-                try {
-                    Socket socket = new Socket(SERVER_IP, PORTA);
-                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while (true) { // loop infinito
+                    try {
+                        Thread.sleep(rand.nextInt(5000)); // delay aleatório entre envios
 
-                    String msg = "Mensagem da thread " + threadId;
-                    out.write(msg + "\n");
-                    out.flush();
+                        Socket socket = new Socket(SERVER_IP, PORTA);
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                    String resp = in.readLine();
-                    System.out.println("Resposta do servidor: " + resp);
+                        String msg = "Thread " + threadId + " envia número aleatório " + rand.nextInt(100);
+                        out.write(msg + "\n");
+                        out.flush();
 
-                    socket.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                        String resp = in.readLine();
+                        System.out.println("Resposta TCP do servidor: " + resp);
+
+                        socket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }).start();
         }
