bc9ae35c74d24d2f24846a7f20c97f991ec824b0
Lucas Oliveira <luckass1088@gmail.com>
2025-09-15 23:48:19 -0300

    Create ClienteUDP_Single.java

---
+++ ClienteUDP_Single.java
@@ -0,0 +1,35 @@
+import java.net.*;
+import java.util.Random;
+
+public class ClienteUDP_Single {
+    private static final String SERVER_IP = "192.168.100.101";
+    private static final int PORTA = 6000;
+    private static final int NUM_MSG = 5;
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress endereco = InetAddress.getByName(SERVER_IP);
+
+            for (int i = 1; i <= NUM_MSG; i++) {
+                String msg = "Mensagem UDP " + i + " valor aleatório " + rand.nextInt(1000);
+                DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), endereco, PORTA);
+                socket.send(packet);
+
+                byte[] buffer = new byte[1024];
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+                socket.receive(reply);
+
+                System.out.println("Resposta UDP: " + new String(reply.getData(), 0, reply.getLength()));
+
+                Thread.sleep(rand.nextInt(3000)); // delay aleatório
+            }
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
