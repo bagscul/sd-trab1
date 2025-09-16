04aaab7a1d83ed8b65a7b4cdd85837a294e64404
Lucas Oliveira <luckass1088@gmail.com>
2025-09-15 23:20:09 -0300

    Update ClienteTCP.java

--- ClienteTCP.java
+++ ClienteTCP.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 public class ClienteTCP {
-    private static final String SERVER_IP = "192.168.100.101";
+    private static final String SERVER_IP = "192.168.0.1";
     private static final int PORTA = 5000;
 
     public static void main(String[] args) {
